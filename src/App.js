427776762574b3724c83be7c756811ff05fc0277
Alibaba <abdulmanafaliu414@gmail.com>
2024-02-20 01:41:09 +0000

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Nav_bar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home_page/Home";
+import Login from "./Components/Forms/User-login/Login";
+//import Signup from "./Components/Forms/User-signup/Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
