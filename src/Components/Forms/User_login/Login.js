5b233935b50f0dc49eb2f5fd4d78fff0bf07280f
Alibaba <abdulmanafaliu414@gmail.com>
2024-02-26 06:30:47 +0000

    Minor changes

--- src/Components/Forms/User_login/Login.js
+++ src/Components/Forms/User_login/Login.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 //import "./frontpage.css";
 import { Link } from "react-router-dom";
 import "./login.css";
 import login from "./HandyLogin.png";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("user Email:", username);
+    console.log("password:", password);
+  };
   return (
     <div className="forms-continer">
       {/* 
@@ -59,56 +66,80 @@ function Login() {
           <h2 style={{ fontFamily: "Sofia" }}>Sign In to HandyPro</h2>
           <p>
             {" "}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            <br /> Donec pharetra rhoncus suscipit.
+            Welcome back to HandyPro! Log in to conveniently manage
+            <br /> your tasks and services and gain access to your account.
+            <br />
           </p>
         </div>
-        <div>
-          <div className="login-forms">
-            <input type="text" placeholder="Username" required /> <br /> <br />
-            <input type="password" placeholder="Password" required />
-          </div>
-          <div className="remember-and-forgot">
+        <form onSubmit={handleLogin}>
+          <div>
             <div>
-              <input type="checkbox" placeholder="remember me" required />
-              <span>Remember me</span>
+              <div className="login-forms">
+                <input
+                  type="text"
+                  placeholder="Username"
+                  required
+                  onChange={(e) => setUsername(e.target.value)}
+                />{" "}
+                <br /> <br />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="remember-and-forgot">
+                <div
+                  className=""
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <div>
+                    <input type="checkbox" />
+                  </div>
+                  <div style={{ marginLeft: "5px" }}>
+                    <span>Remember me</span>
+                  </div>
+                </div>
+                <div>
+                  <Link to="">
+                    <span>Forgot Password?</span>
+                  </Link>
+                </div>
+              </div>
             </div>
-            <div>
-              <Link to="">
-                <span>Forgot Password?</span>
-              </Link>
-            </div>
+            <button
+              style={{
+                width: "100%",
+                marginTop: "3em",
+                border: "none",
+                padding: "10px 10px",
+                backgroundColor: "#6C63FF",
+                borderRadius: "5px",
+                color: "white",
+              }}
+            >
+              Log In
+            </button>
           </div>
-        </div>
-        <button
-          style={{
-            width: "100%",
-            marginTop: "3em",
-            border: "none",
-            padding: "10px 10px",
-            backgroundColor: "#6C63FF",
-            borderRadius: "5px",
-            color: "white",
-          }}
-        >
-          Log In
-        </button>
+        </form>
+
         <p>Or sign In with</p>
         <div className="external-login">
           <div className="google">
-            <a href="">
-              <i class="fa fa-google" aria-hidden="true"></i>
-            </a>
+            <Link href="">
+              <i className="fa fa-google" aria-hidden="true"></i>
+            </Link>
           </div>
           <div className="twitter">
-            <a href="">
-              <i class="fa fa-twitter" aria-hidden="true"></i>
-            </a>
+            <Link href="">
+              <i className="fa fa-twitter" aria-hidden="true"></i>
+            </Link>
           </div>
           <div className="facebook">
-            <a href="">
-              <i class="fa fa-facebook" aria-hidden="true"></i>
-            </a>
+            <Link href="">
+              <i className="fa fa-facebook" aria-hidden="true"></i>
+            </Link>
           </div>
         </div>
       </div>
