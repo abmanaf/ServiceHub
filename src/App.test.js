60be02e84c1ae4fd93b81f9a83d048039e3d40f0
abmanaf <aliu.manaf@amalitech.com>
2025-01-27 20:22:19 +0000

    refactor: changes made

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jsdom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Should render the HandyPro website', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to HandyPro/i);
   expect(linkElement).toBeInTheDocument();
 });
